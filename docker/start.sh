148616244dc29df5a5dce9dcc02e653c3a18bc55
Juho Friman <juho.friman@solita.fi>
2017-10-12 16:10:42 +0300

    Created separate destroy.sh script
    Script simply destroys existing image

--- docker/start.sh
+++ docker/start.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -o errexit
 
-stop_old_container () {
-    if docker kill solita-blag; then
-        docker rm solita-blag
-    fi
-}
-
 main () {
-    stop_old_container
     docker run -it -v `pwd`:/solita-blag --name solita-blag --rm -p 4444:4000 solita-blag
 }
 
