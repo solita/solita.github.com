bc7f76bababe9c36d7c2078bcced61060946f675
Jari Hanhela <jarihanhela@gmail.com>
2018-02-23 14:06:09 +0200

    Remove temp placeholder

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@
 
     function get_gravatar(email, size) {
         size = size || AVATAR_SIZE;
-        var defaultImage = 'https://preview.ibb.co/cSmCtH/solita_person_placeholder.png';
+        var defaultImage = 'http://dev.solita.fi/img/solita-person-placeholder.png';
         return 'https://www.gravatar.com/avatar/' + email + '.jpg?size=' + size + '&default=' + defaultImage;
     }
 
