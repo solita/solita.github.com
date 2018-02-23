728981b316347607162866e59d3b30ad8f0b4542
Jari Hanhela <jarihanhela@gmail.com>
2018-02-23 14:05:27 +0200

    Use temp placeholder for testing purposes

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@
 
     function get_gravatar(email, size) {
         size = size || AVATAR_SIZE;
-        var defaultImage = 'http://dev.solita.fi/img/solita-person-placeholder.png';
+        var defaultImage = 'https://preview.ibb.co/cSmCtH/solita_person_placeholder.png';
         return 'https://www.gravatar.com/avatar/' + email + '.jpg?size=' + size + '&default=' + defaultImage;
     }
 
