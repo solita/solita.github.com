e564773c4814e3bd8d0269abd81703db65cf9565
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2024-10-10 14:44:30 +0300

    Add proper links and author info

--- authors.clj
+++ authors.clj
@@ -1,29 +1,49 @@
 #!/usr/bin/env bb
 (require '[clojure.string :as str]
-         '[cheshire.core :as json])
+         '[cheshire.core :as json]
+         '[clj-yaml.core :as yaml])
+
+(defn file->relative-url
+  "Convert file like:
+  _posts/1984-04-21-war-with-eurasia.md
+
+to a jekyll page link:
+  /1984/04/21/war-with-eurasia.html"
+  [f]
+  (apply format "/%s/%s/%s/%s.html"
+         (drop 1 (re-find #"^(\d+)-(\d+)-(\d+)-(.*).md$" (.getName f)))))
+
+(def author-by-handle
+  (let [authors (-> "_config.yml" slurp yaml/parse-string :authors)]
+    (fn [author-name]
+      (authors (keyword author-name)))))
+
+(defn yaml-header [post-file]
+  (->> post-file slurp str/split-lines
+       (drop 1)
+       (take-while #(not= "---" %))
+       (str/join "\n")
+       yaml/parse-string))
 
 (def author->posts
   (->> (java.io.File. "_posts")
        .listFiles
        (filter #(str/ends-with? (.getName %) ".md"))
        (mapcat (fn [f]
-                 (let [contents (slurp f)
-                       [_ title] (re-find #"(?im)^title: (.*)$" contents)
-                       authors (-> (re-find #"(?im)^.*author: (.*)$" contents)
-                                   second
-                                   (str/split #", "))]
+                 (let [{:keys [title author]} (yaml-header f)
+                       authors (map str/trim (str/split author #","))]
                    (for [author authors]
-                     {:title title :author author :post (str "_posts/" (.getName f))}))))
+                     {:title title :author author :url (file->relative-url f)}))))
        (group-by :author)
-       (into {} (map (fn [[author posts]]
-                       ;; sort by date (part of filename)
-                       [author (reverse (sort-by :post posts))])))
-       (sort-by (comp count second))
+       (map (fn [[author posts]]
+              ;; sort by date (part of filename)
+              (assoc (author-by-handle author)
+                     :handle author
+                     :posts (->> posts
+                                 (map #(dissoc % :author))
+                                 (sort-by :url)
+                                 reverse))))
+       (sort-by (comp count :posts))
        reverse))
 
-#_(->> author-counts
-     (map (fn [[author count]] {:author author :count count}))
-     clojure.pprint/print-table)
-
 (spit "_data/authors.json" (json/generate-string author->posts))
-;(println "total authors: " (count author-counts))
